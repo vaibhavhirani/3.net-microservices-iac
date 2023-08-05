d84978f858ac8fdc68022b75b14a750949831b39
vaibhavhirani <vaibhavhirani@gmail.com>
2023-08-05 17:58:16 +0530

    creates deployment slots

--- modules/azure-app-service-docker/appService.tf
+++ modules/azure-app-service-docker/appService.tf
@@ -3,8 +3,26 @@ resource "azurerm_service_plan" "app_service_plan" {
   location            = var.region
   resource_group_name = var.resource_group_name
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "P1v2"
   depends_on = [
     azurerm_resource_group.rg
   ]
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+  name                  = format("%s%s", var.app_service_name, "3net")
+  location              = var.region
+  resource_group_name   = var.resource_group_name
+  service_plan_id       = azurerm_service_plan.app_service_plan.id
+  https_only            = true
+  site_config { 
+    minimum_tls_version = "1.2"
+  }
+  
+}
+
+resource "azurerm_linux_web_app_slot" "staging_slot" {
+  name           = "staging"
+  app_service_id = azurerm_linux_web_app.webapp.id
+  site_config {}
 }
