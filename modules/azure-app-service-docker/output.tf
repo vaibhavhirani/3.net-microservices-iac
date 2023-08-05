07d4d5db978b33c0a536b3b267f3457e5b004832
vaibhavhirani <vaibhavhirani@gmail.com>
2023-08-05 14:26:32 +0530

    outputs docker registry

--- modules/azure-app-service-docker/output.tf
+++ modules/azure-app-service-docker/output.tf
@@ -1,11 +1,15 @@
 output "azure_container_registry" {
     value = azurerm_container_registry.acr.login_server
+    # sensitive = true
 }
 output "azure_container_registry_admin_username" {
     value = azurerm_container_registry.acr.admin_username
+    sensitive = true
 }
 output "azure_container_registry_admin_password" {
     value = azurerm_container_registry.acr.admin_password
+    sensitive = true
 }
+
 
 
