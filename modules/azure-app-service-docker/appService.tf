a08037165ae605ba9b3328c3bb1c28a1af494cb6
vaibhavhirani <vaibhavhirani@gmail.com>
2023-08-05 13:58:51 +0530

    adds terraform backend

--- modules/azure-app-service-docker/appService.tf
+++ modules/azure-app-service-docker/appService.tf
@@ -8,11 +8,3 @@ resource "azurerm_service_plan" "app_service_plan" {
     azurerm_resource_group.rg
   ]
 }
-
-
-# resource "azurerm_linux_web_app" "app_service" {
-#   name                = format("%s-%s", var.app_service_name, "appservice")
-#   location            = var.region
-#   resource_group_name = var.resource_group_name
-#   service_plan_id  = azurerm_service_plan.app_service_plan.id
-# }
