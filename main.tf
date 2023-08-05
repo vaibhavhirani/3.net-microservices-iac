28504507da9082bfd58c6fadf3de29e12818d0d4
vaibhavhirani <vaibhavhirani@gmail.com>
2023-08-05 14:24:34 +0530

    outputs main,tf

--- main.tf
+++ main.tf
@@ -16,3 +16,15 @@ provider "azurerm" {
 module "my_eks_module" {
   source = "./modules/azure-app-service-docker"  
 }
+
+output "azure_container_registry" {
+    value = module.my_eks_module.azure_container_registry
+}
+output "azure_container_registry_admin_username" {
+    value = module.my_eks_module.azure_container_registry_admin_username
+}
+output "azure_container_registry_admin_password" {
+    value = module.my_eks_module.azure_container_registry_admin_password
+}
+
+
