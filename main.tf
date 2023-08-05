8ee050667d09955380fca49b40beed48751c8dd8
vaibhavhirani <vaibhavhirani@gmail.com>
2023-08-05 14:44:11 +0530

    initialize backend

--- main.tf
+++ main.tf
@@ -7,11 +7,42 @@ terraform {
       version = ">= 3.0, < 4.0"
     }
   }
+  backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name =  "terraformbackenddotnet"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "tfstate" {
+  name     = "tfstate"
+  location = "Central India"
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "terraformbackenddotnet"
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_blob_public_access = false
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
+
 
 module "my_eks_module" {
   source = "./modules/azure-app-service-docker"  
