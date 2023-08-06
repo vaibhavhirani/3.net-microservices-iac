a838377f6ced69e7c5bd5a0c2cd2ba01b0d0b6c5
vaibhavhirani <vaibhavhirani@gmail.com>
2023-08-06 21:01:51 +0530

    documents module consumption

--- main.tf
+++ main.tf
@@ -6,6 +6,8 @@ terraform {
       source  = "hashicorp/azurerm"
     }
   }
+
+  # sets the backend of tfstate files on Azure using below specs
   backend "azurerm" {
       resource_group_name  = "tfstate"
       storage_account_name =  "storageaccounttfstate"
@@ -18,11 +20,13 @@ provider "azurerm" {
   features {}
 }
 
+# Azure Resource Group Required for keeping tfstate backend resource together
 resource "azurerm_resource_group" "tfstate" {
   name     = "tfstate"
   location = "Central India"
 }
 
+# Storage Account keeps tfstate container
 resource "azurerm_storage_account" "tfstate" {
   name                     = "storageaccounttfstate"
   resource_group_name      = azurerm_resource_group.tfstate.name
@@ -30,22 +34,25 @@ resource "azurerm_storage_account" "tfstate" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   # allow_blob_public_access = false
-
   tags = {
     environment = "staging"
   }
 }
 
+
+# Entity containing the tfstates.
 resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstatecontainer"
   storage_account_name  = azurerm_storage_account.tfstate.name
   container_access_type = "private"
 }
 
+# Consuming azure-app-service-docker module.
 module "my_az_module" {
   source = "./modules/azure-app-service-docker"  
 }
 
+# Output variables
 output "azure_container_registry" {
     value = module.my_az_module.azure_container_registry
     # sensitive = true
